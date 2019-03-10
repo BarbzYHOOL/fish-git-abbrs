f862095c770cbf132d4eba3d55208edc8e596aac
BarbzYHOOL <professor_barbz@gmx.com>
2019-03-10 17:33:33 +0100

    feat: Improve speed with global abbrs
    Instead of universal abbrs

--- init.fish
+++ init.fish
@@ -6,133 +6,133 @@
 # * $dependencies  package dependencies
 
 ## git
-abbr -a g          git
+abbr --add --global g          git
 
 ## git add
-abbr -a ga         git add
-abbr -a gaa        git add --all
-abbr -a gapa       git add --patch
+abbr --add --global ga         git add
+abbr --add --global gaa        git add --all
+abbr --add --global gapa       git add --patch
 
 ## git branch
-abbr -a gb         git branch -vv
-abbr -a gba        git branch -a -vv
+abbr --add --global gb         git branch -vv
+abbr --add --global gba        git branch -a -vv
 
 ## git commit
-abbr -a gc         git commit -v
-abbr -a gc!        git commit -v --amend
-abbr -a gc!n       git commit -v --amend --no-edit
-abbr -a gca        git commit -v -a
-abbr -a gca!       git commit -v -a --amend
-abbr -a gca!n      git commit -v -a --amend --no-edit
-abbr -a gcmsg      git commit -m
+abbr --add --global gc         git commit -v
+abbr --add --global gc!        git commit -v --amend
+abbr --add --global gc!n       git commit -v --amend --no-edit
+abbr --add --global gca        git commit -v -a
+abbr --add --global gca!       git commit -v -a --amend
+abbr --add --global gca!n      git commit -v -a --amend --no-edit
+abbr --add --global gcmsg      git commit -m
 
 ## git clone
-abbr -a gcl        git clone
-abbr -a gcld       git clone --depth
-abbr -a gcl1       git clone --depth 1				# clone only 1 commit long history
-abbr -a gclm	   git clone --branch master		# clone only master branch
-abbr -a gcls	   git clone --depth 1 -b master 	# mix of the two above (gcls = git clone short)
+abbr --add --global gcl        git clone
+abbr --add --global gcld       git clone --depth
+abbr --add --global gcl1       git clone --depth 1				# clone only 1 commit long history
+abbr --add --global gclm	   git clone --branch master		# clone only master branch
+abbr --add --global gcls	   git clone --depth 1 -b master 	# mix of the two above (gcls = git clone short)
 
 ## git shortlog
-abbr -a gcount     git shortlog -sn
+abbr --add --global gcount     git shortlog -sn
 
 ## git cherry pick
-abbr -a gcp        git cherry-pick
+abbr --add --global gcp        git cherry-pick
 
 ## git diff
-abbr -a gd         git diff
-abbr -a gdca       git diff --cached
+abbr --add --global gd         git diff
+abbr --add --global gdca       git diff --cached
 
 ## git fetch
-abbr -a gf         git fetch
-abbr -a gfa        git fetch --all --prune
-abbr -a gfo        git fetch origin
+abbr --add --global gf         git fetch
+abbr --add --global gfa        git fetch --all --prune
+abbr --add --global gfo        git fetch origin
 
 ## git pull
-abbr -a gpl        git pull
+abbr --add --global gpl        git pull
 
 ## git push
-abbr -a gps        git push
+abbr --add --global gps        git push
 
 ## git show
-abbr -a gsh        git show
+abbr --add --global gsh        git show
 
 ## git merge
-abbr -a gm         git merge
-abbr -a gmt        git mergetool --no-prompt
+abbr --add --global gm         git merge
+abbr --add --global gmt        git mergetool --no-prompt
 
 ## git rebase
-abbr -a grba       git rebase --abort
-abbr -a grbc       git rebase --continue
-abbr -a grbi       git rebase --interactive
-abbr -a grbm       git rebase master
-abbr -a grbs       git rebase --skip
+abbr --add --global grba       git rebase --abort
+abbr --add --global grbc       git rebase --continue
+abbr --add --global grbi       git rebase --interactive
+abbr --add --global grbm       git rebase master
+abbr --add --global grbs       git rebase --skip
 
 ## git reset
-abbr -a grh        git reset HEAD
-abbr -a grhh       git reset HEAD --hard
+abbr --add --global grh        git reset HEAD
+abbr --add --global grhh       git reset HEAD --hard
 
 ## git remote
-abbr -a gr         git remote -vv
-abbr -a gra        git remote add
-abbr -a grs        git remote set-url
-abbr -a grmv       git remote rename
-abbr -a grrm       git remote remove
-abbr -a grup       git remote update
+abbr --add --global gr         git remote -vv
+abbr --add --global gra        git remote add
+abbr --add --global grs        git remote set-url
+abbr --add --global grmv       git remote rename
+abbr --add --global grrm       git remote remove
+abbr --add --global grup       git remote update
 
 ## git svn
-abbr -a gsd        git svn dcommit
-abbr -a gsr        git svn rebase
+abbr --add --global gsd        git svn dcommit
+abbr --add --global gsr        git svn rebase
 
 ## git status
-abbr -a gst        git status
-abbr -a gsv        git status -v
-abbr -a gsvv       git status -vv
-abbr -a gss        git status -s
+abbr --add --global gst        git status
+abbr --add --global gsv        git status -v
+abbr --add --global gsvv       git status -vv
+abbr --add --global gss        git status -s
 
 ## git stash
-abbr -a gsta       git stash
-abbr -a gstd       git stash drop
-abbr -a gstp       git stash pop
-abbr -a gsts       git stash show --text
-abbr -a gunignore  git update-index --no-assume-unchanged
-abbr -a gup        git pull --rebase
-abbr -a gwch       git whatchanged -p --abbrev-commit --pretty=medium
+abbr --add --global gsta       git stash
+abbr --add --global gstd       git stash drop
+abbr --add --global gstp       git stash pop
+abbr --add --global gsts       git stash show --text
+abbr --add --global gunignore  git update-index --no-assume-unchanged
+abbr --add --global gup        git pull --rebase
+abbr --add --global gwch       git whatchanged -p --abbrev-commit --pretty=medium
 
 ## git checkout abbreviations
-abbr -a gch        git checkout
-# abbr -a gch.       git checkout .
-abbr -a gchb       git checkout -b
-abbr -a gchd       git checkout develop
-abbr -a gchm       git checkout master
+abbr --add --global gch        git checkout
+# abbr --add --global gch.       git checkout .
+abbr --add --global gchb       git checkout -b
+abbr --add --global gchd       git checkout develop
+abbr --add --global gchm       git checkout master
 
 ## git log
-abbr -a glg        git log --stat --max-count=10
-abbr -a glgg       git log --graph --max-count=10
-abbr -a glgga      git log --graph --decorate --all
-abbr -a glo        "git log --pretty=format:'%C(yellow)%h %C(red)%ad %C(blue)%an%C(green)%d %C(reset)%s %C(white)%N' --date=short -n 5"
-abbr -a glog       git log --oneline --decorate --color --graph
+abbr --add --global glg        git log --stat --max-count=10
+abbr --add --global glgg       git log --graph --max-count=10
+abbr --add --global glgga      git log --graph --decorate --all
+abbr --add --global glo        "git log --pretty=format:'%C(yellow)%h %C(red)%ad %C(blue)%an%C(green)%d %C(reset)%s %C(white)%N' --date=short -n 5"
+abbr --add --global glog       git log --oneline --decorate --color --graph
 
 ## git notes
-abbr -a gn 		   git notes add -m 
-abbr -a gns		   git notes show
+abbr --add --global gn 		   git notes add -m
+abbr --add --global gns		   git notes show
 
 ## jack (requires jack)
-abbr -a j          jack
-abbr -a jlg        jack --stat --max-count=10
-abbr -a jlgg       jack --graph --max-count=10
-abbr -a jlgga      jack --graph --decorate --all
-abbr -a jlo        "jack --pretty=format:'%C(yellow)%h %C(red)%ad %C(blue)%an%C(green)%d %C(reset)%s %C(white)%N' --date=short"
-abbr -a jlog       jack --oneline --decorate --color --graph
+abbr --add --global j          jack
+abbr --add --global jlg        jack --stat --max-count=10
+abbr --add --global jlgg       jack --graph --max-count=10
+abbr --add --global jlgga      jack --graph --decorate --all
+abbr --add --global jlo        "jack --pretty=format:'%C(yellow)%h %C(red)%ad %C(blue)%an%C(green)%d %C(reset)%s %C(white)%N' --date=short"
+abbr --add --global jlog       jack --oneline --decorate --color --graph
 
 
 
 ## others
-abbr -a gff        git flow feature
-abbr -a gfr        git flow release
-abbr -a gfh        git flow hotfix
-abbr -a gfs        git flow support
+abbr --add --global gff        git flow feature
+abbr --add --global gfr        git flow release
+abbr --add --global gfh        git flow hotfix
+abbr --add --global gfs        git flow support
 
-abbr -a gignore    git update-index --assume-unchanged
+abbr --add --global gignore    git update-index --assume-unchanged
 
-abbr -a gk         gitk --all --branches
+abbr --add --global gk         gitk --all --branches
